785c050304fc823049724d2ae1c71a973dfbff83
gaopeng3 <gaopeng3@kingsoft.com>
2019-07-20 01:22:11 +0800

    16

--- RGImageSticker/RGImageSticker/ViewController.m
+++ RGImageSticker/RGImageSticker/ViewController.m
@@ -22,7 +22,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
+    // Do any additional2
     
     //1
     [self.view addSubview:self.photoCollectionView];
